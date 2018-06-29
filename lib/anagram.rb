b879698cc1969ed19b6927165db323f8f9f8f8e7
github username <github email address>
2018-06-29 16:22:16 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word, :breakdown, :broken_word
+
+def initialize(word)
+@word = word
+@broken_word = split(@word)
+end
+
+def match(phrase)
+  match = break_it_down(phrase).select {|word| word.sort.join == @broken_word.sort.join}
+  match.map {|word| word.join}
+
+end
+
+def break_it_down(phrase)
+  @breakdown = phrase.map {|word| split(word)}
+end
+
+def split(word)
+  word.split(//)
+end
+
+end
