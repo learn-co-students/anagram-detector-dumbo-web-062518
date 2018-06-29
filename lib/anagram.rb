6a207ff03634013261fcd6c578a1335b0a9fea85
Dan Chung <danetix@gmail.com>
2018-06-29 12:22:00 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(data)
+    sorted_word = @word.chars.sort.join
+    data.select do |compare|
+      compare.chars.sort.join == sorted_word
+    end
+  end
+
+  # binding.pry
+end
